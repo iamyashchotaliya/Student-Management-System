ba92ac63c7d17e212d2b06bd33d55015beb00059
Yash Chotaliya <ychotaliya5@gmail.com>
2025-12-01 15:27:01 +0530

    Remove PORT from env for Railway

--- backend/server.js
+++ backend/server.js
@@ -24,5 +24,6 @@ app.use('/api/students', require('./routes/studentRoutes'));
 // ✅ Attendance routes (ye line add karo)
 app.use('/api/attendance', require('./routes/attendanceRoutes'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} ✅`));
+
